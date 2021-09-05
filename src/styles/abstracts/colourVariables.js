e6f53595108ac44a46b0d526165006d40da3c78a
allicce <stetiarova.alzbeta@gmail.com>
2021-09-05 20:13:09 +0200

    style(variables) add colour palette

---
+++ src/styles/abstracts/colourVariables.js
@@ -0,0 +1,16 @@
+export const GREY_1 = "#F2F2F2";
+export const GREY_2 = "#E0E0E0";
+export const GREY_3 = "#BDBDBD";
+export const GREY_4 = "#828282";
+export const GREY_5 = "#4F4F4F";
+export const GREY_6 = "#333333";
+
+export const PRIMARY_COLOR = "#00BCFA";
+export const PRIMARY_COLOR_LIGHT = "#4BCFFB";
+export const PRIMARY_COLOR_DARK = "#005C7A";
+export const PRIMARY_COLOR_DARKER = "#0095C7";
+export const BLUE_COLOR_DARK = "#25657A";
+
+export const ERROR_RED_COLOR = "#FF8F8F";
+export const RED_COLOR_DARK = "#EB5757";
+
